5998c81b44b928c4f4bb5f9c251750b467c1aa9c
Administrator <administrator@c1r009n01.amm.vsu.ru>
2016-10-08 14:50:09 +0300

    playing around with gesture recognizers

--- UIGestureRecognizerDemo/ViewController.swift
+++ UIGestureRecognizerDemo/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var image: UIImageView!
+    
+    var tapGesture: UITapGestureRecognizer!
+    var doubleTapGesture: UITapGestureRecognizer!
+    var rotationGesture: UIRotationGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(_:)))
+        tapGesture.delegate = self
+        doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.didDoubleTap(_:)))
+        doubleTapGesture.numberOfTapsRequired = 2
+        rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.didRotate(_:)))
+        
+        let swipe = UISwipeGestureRecognizer()
+        image.addGestureRecognizer(tapGesture)
+        image.addGestureRecognizer(rotationGesture)
+        image.addGestureRecognizer(doubleTapGesture)
+        
     }
 
+    
+    func didTap(_ recognizer: UITapGestureRecognizer) {
+        print("did tap")
+    }
+    
+    func didDoubleTap(_ recognizer: UITapGestureRecognizer) {
+        print("did double tap")
+    }
+    
+    func didRotate(_ recognizer: UIRotationGestureRecognizer) {
+        image.transform = image.transform.rotated(by: recognizer.rotation)
+        recognizer.rotation = 0.0
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let g1 = gestureRecognizer as? UITapGestureRecognizer,
+            let g2 = otherGestureRecognizer as? UITapGestureRecognizer {
+            if g1.numberOfTapsRequired < g2.numberOfTapsRequired {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
